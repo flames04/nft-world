a5750794d58dbcadda2d596a3d1a1c1628d799f3
Rocco P <roccopan@gmail.com>
2021-12-07 10:40:35 -0500

    Built a single object of data.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const allProducts = [
+  {
+    prodId: 1234,
+    name: `Crypto Punk #1234`,
+    image: `1.png`,
+    price: {
+      was: 11.2,
+      is: 9.9
+    },
+    desc: `Here is a shot of this product that might entice a user to click and add it to their cart.`,
+    rating: 4.4,
+    available: true,
+    yourFav: false
+  }
+]
